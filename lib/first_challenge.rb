50eeb59d3b532ca7da5d8f164d4b65b44d426f00
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 13:24:43 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,7 +7,10 @@ def first_challenge
        :hero_friends => []
      },
      :capulet => {
-       :patriarch => {}
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
      }
 
 
