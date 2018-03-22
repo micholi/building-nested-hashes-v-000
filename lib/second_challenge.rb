e1629d2d9bf2900b5dda6dbe48e15cdb070a210c
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 13:27:00 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -15,21 +15,3 @@ def second_challenge
 
   
 end
-
-def first_challenge
-   epic_tragedy = {
-     :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-     },
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-       
-     }
-   }
-end
